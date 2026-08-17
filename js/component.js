b9a3325cc2aa5066c40f8a179f265ff694780b5a
Seongjoo_Moon <yellowmoon17171771@gmail.com>
2026-08-17 15:49:50 +0200

    fix href

--- js/component.js
+++ js/component.js
@@ -6,11 +6,11 @@ class MainNav extends HTMLElement {
                 <a class="site-title" href="./index.html">Seongjoo Moon</a>
                 <button class="hamburger">☰</button>
                 <ul class="nav-list">
-                    <li><a href="./pages/about.html">about</a></li>
-                    <li><a href="./pages/news.html">news</a></li>
-                    <li><a href="./pages/works.html">works</a></li>
-                    <li><a href="./pages/rabbit-hole.html">rabbit hole</a></li>
-                    <li><a href="./pages/blog.html">blog</a></li>
+                    <li><a href="https://yellow-moonlight.github.io/Portfolio/pages/about.html">about</a></li>
+                    <li><a href="https://yellow-moonlight.github.io/Portfolio/pages/news.html">news</a></li>
+                    <li><a href="https://yellow-moonlight.github.io/Portfolio/pages/works.html">works</a></li>
+                    <li><a href="https://yellow-moonlight.github.io/Portfolio/pages/rabbit-hole.html">rabbit hole</a></li>
+                    <li><a href="https://yellow-moonlight.github.io/Portfolio/pages/blog.html">blog</a></li>
                 </ul>
             </div>
               </nav>
--- js/technical_component.js
+++ js/technical_component.js
@@ -3,7 +3,7 @@ class MainNav extends HTMLElement {
     this.innerHTML = `
         <nav class="nav-container">
             <div class="nav-inner">
-                <a class="site-title" href="/pages/technical_works.html">Seongjoo Moon Portfolio for Prototyping & Production</a>
+                <a class="site-title" href="https://yellow-moonlight.github.io/Portfolio/pages/technical_works.html">Seongjoo Moon Portfolio for Prototyping & Production</a>
             </div>
               </nav>
         `;
