22517c503b482e57dd34698e9a83d6097b1fd758
Seongjoo_Moon <yellowmoon17171771@gmail.com>
2026-08-17 15:45:26 +0200

    fix error

--- js/component.js
+++ js/component.js
@@ -3,14 +3,14 @@ class MainNav extends HTMLElement {
     this.innerHTML = `
         <nav class="nav-container">
             <div class="nav-inner">
-                <a class="site-title" href="/index.html">Seongjoo Moon</a>
+                <a class="site-title" href="./index.html">Seongjoo Moon</a>
                 <button class="hamburger">☰</button>
                 <ul class="nav-list">
-                    <li><a href="/pages/about.html">about</a></li>
-                    <li><a href="/pages/news.html">news</a></li>
-                    <li><a href="/pages/works.html">works</a></li>
-                    <li><a href="/pages/rabbit-hole.html">rabbit hole</a></li>
-                    <li><a href="/pages/blog.html">blog</a></li>
+                    <li><a href="./pages/about.html">about</a></li>
+                    <li><a href="./pages/news.html">news</a></li>
+                    <li><a href="./pages/works.html">works</a></li>
+                    <li><a href="./pages/rabbit-hole.html">rabbit hole</a></li>
+                    <li><a href="./pages/blog.html">blog</a></li>
                 </ul>
             </div>
               </nav>
